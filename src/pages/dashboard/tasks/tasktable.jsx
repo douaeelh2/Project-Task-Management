6950a7a7a3af5663003af26c7eeb9878bfc0e6b8
douaeelh2 <douaeelh2@gmail.com>
2023-11-23 19:29:26 +0100

    other commit

--- src/pages/dashboard/tasks/tasktable.jsx
+++ src/pages/dashboard/tasks/tasktable.jsx
@@ -1,103 +1,163 @@
+import React from "react";
 import {
-    Card,
-    CardHeader,
-    CardBody,
-    Typography,
-    Avatar,
-    Chip,
-    Tooltip,
-    Progress,
-  } from "@material-tailwind/react";
-  import { EllipsisVerticalIcon } from "@heroicons/react/24/outline";
-  import { projectsTableData } from "@/data";
-  
-  export function TaskTable() {
-    return (
+  Typography,
+  Card,
+  CardHeader,
+  CardBody,
+  IconButton,
+  Menu,
+  MenuHandler,
+  MenuList,
+  MenuItem,
+  Avatar,
+  Tooltip,
+  Progress,
+  Input,
+  Chip,
+  Button
+} from "@material-tailwind/react";
+import {
+  EllipsisVerticalIcon,
+  ArrowUpIcon,
+} from "@heroicons/react/24/outline";
+import { StatisticsCard } from "@/widgets/cards";
+import { StatisticsChart } from "@/widgets/charts";
+import {
+  statisticsCardsData,
+  statisticsChartsData,
+  projectsTableData,
+  ordersOverviewData,
+} from "@/data";
+import { CheckCircleIcon, ClockIcon,PencilSquareIcon,EyeIcon ,TrashIcon,MagnifyingGlassIcon  } from "@heroicons/react/24/solid";
+import { Link } from "react-router-dom";
+
+export function TaskTable() {
+  return (
+
       <div className="mt-12 mb-8 flex flex-col gap-12">
         <Card>
-          <CardHeader variant="gradient" color="gray" className="mb-8 p-6">
-            <Typography variant="h6" color="white">
-              Tasks Table
-            </Typography>
+          <CardHeader
+            floated={false}
+            shadow={false}
+            color="transparent"
+            className="m-0 flex items-center justify-between p-6"
+          >
+            <div className="flex items-center justify-between gap-4">
+              <Typography variant="h5" color="blue-gray" className="mb-1">
+                Projects Table
+              </Typography>
+            </div>
+            <div className="flex items-center justify-between mr-5 gap-4">
+            <Input label="Search By Name"/>
+            <Link to=".">
+                <IconButton variant="gradient" color="black">
+                  <MagnifyingGlassIcon className="h-5 w-5 text-white" />
+                </IconButton>
+            </Link>
+            </div>
+            
           </CardHeader>
           <CardBody className="overflow-x-scroll px-0 pt-0 pb-2">
-            <table className="w-full min-w-[640px] table-auto">
+          <table className="w-full min-w-[640px] table-auto">
               <thead>
                 <tr>
-                  {["author", "function", "status", "employed", ""].map((el) => (
-                    <th
-                      key={el}
-                      className="border-b border-blue-gray-50 py-3 px-5 text-left"
-                    >
-                      <Typography
-                        variant="small"
-                        className="text-[11px] font-bold uppercase text-blue-gray-400"
+                  {["Projects", "Members", "Duration", "Status" , "Show", "Edit", "Delete"].map(
+                    (el) => (
+                      <th
+                        key={el}
+                        className="border-b border-blue-gray-50 py-3 px-6 text-left"
                       >
-                        {el}
-                      </Typography>
-                    </th>
-                  ))}
+                        <Typography
+                          variant="small"
+                          className="text-[11px] font-medium uppercase text-blue-gray-400"
+                        >
+                          {el}
+                        </Typography>
+                      </th>
+                    )
+                  )}
                 </tr>
               </thead>
               <tbody>
                 {projectsTableData.map(
-                  ({ img, name, email, job, status, date }, key) => {
-                    const className = `py-3 px-5 ${
+                  ({project, members, duration, status , manage }, key) => {
+                    const className = `py-4 px-5 ${
                       key === projectsTableData.length - 1
                         ? ""
                         : "border-b border-blue-gray-50"
                     }`;
-  
+
                     return (
-                      <tr key={name}>
+                      <tr key={project}>
                         <td className={className}>
                           <div className="flex items-center gap-4">
-                            <Avatar src={img} alt={name} size="sm" variant="rounded" />
-                            <div>
-                              <Typography
-                                variant="small"
-                                color="blue-gray"
-                                className="font-semibold"
-                              >
-                                {name}
-                              </Typography>
-                              <Typography className="text-xs font-normal text-blue-gray-500">
-                                {email}
-                              </Typography>
-                            </div>
+                            <Typography
+                              variant="small"
+                              color="blue-gray"
+                              className="font-bold"
+                            >
+                              {project}
+                            </Typography>
                           </div>
                         </td>
                         <td className={className}>
-                          <Typography className="text-xs font-semibold text-blue-gray-600">
-                            {job[0]}
-                          </Typography>
-                          <Typography className="text-xs font-normal text-blue-gray-500">
-                            {job[1]}
-                          </Typography>
-                        </td>
-                        <td className={className}>
-                          <Chip
-                            variant="gradient"
-                            color={status ? "green" : "blue-gray"}
-                            value={status ? "Completed" : "In Progress"}
-                            className="py-0.5 px-2 text-[11px] font-medium w-fit"
-                          />
-                        </td>
-                        <td className={className}>
-                          <Typography className="text-xs font-semibold text-blue-gray-600">
-                            {date}
-                          </Typography>
+                          {members.map(({ img, name }, key) => (
+                            <Tooltip key={project} content={project}>
+                              <Avatar
+                                src={img}
+                                alt={project}
+                                size="xs"
+                                variant="circular"
+                                className={`cursor-pointer border-2 border-white ${
+                                  key === 0 ? "" : "-ml-2.5"
+                                }`}
+                              />
+                            </Tooltip>
+                          ))}
                         </td>
                         <td className={className}>
                           <Typography
-                            as="a"
-                            href="#"
-                            className="text-xs font-semibold text-blue-gray-600"
+                            variant="small"
+                            className="text-xs font-medium text-blue-gray-600"
                           >
-                            Edit
+                            {duration}
                           </Typography>
                         </td>
+                        <td className={className}>
+                        <Chip
+                          variant="gradient"
+                          color={status ? "green" : "blue-gray"}
+                          value={status ? "Completed" : "In Progress"}
+                          className="py-0.5 px-2 text-[11px] font-medium w-fit"
+                        />
+                      </td>
+                      <td className={className}>
+                        <Link to=".">
+                          <IconButton variant="text" color="blue-gray">
+                          <EyeIcon className="h-5 w-5 text-black" />
+                          </IconButton>
+                        </Link>
+                      
+                      </td>
+                      <td className={className}>
+                        <Link to=".">
+                          <IconButton variant="text" color="blue-gray">
+                          <PencilSquareIcon className="h-5 w-5 text-black" />
+                          </IconButton>
+                        </Link>
+                      
+                      </td>
+                      <td className={className}>
+                        <Link to=".">
+                          <IconButton variant="text" color="blue-gray">
+                          <TrashIcon className="h-5 w-5 text-black" />
+                          </IconButton>
+                        </Link>
+                      
+                      </td>
+                        
                       </tr>
+                      
                     );
                   }
                 )}
@@ -107,8 +167,9 @@ import {
         </Card>
         
       </div>
-    );
-  }
-  
-  export default TaskTable;
-  
+
+      
+  );
+}
+
+export default TaskTable;
