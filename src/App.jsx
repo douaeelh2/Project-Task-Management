c5e4d89ba639528b9b5371cd18b70f9a78390e4b
Salma EL HASSNAOUI <elhassnaouisalma@gmail.com>
2023-11-26 22:27:39 +0100

    sign in sign up & afficher sign in  a la place de dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,11 @@ function App() {
     <Routes>
       <Route path="/dashboard/*" element={<Dashboard />} />
       <Route path="/auth/*" element={<Auth />} />
-      <Route path="*" element={<Navigate to="/dashboard/home" replace />} />
+      {/* <Route path="*" element={<Navigate to="/dashboard/home" replace />} /> */}
+      <Route path="*" element={<Navigate to="/auth/sign-in" replace />} />
+
     </Routes>
   );
 }
-
 
 export default App;
